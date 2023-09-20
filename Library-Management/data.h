c30e434da0601646bee0a8781d80e0210defb4d6
EdwardDelRosario <edfrancisdelrosario@gmail.com>
2023-09-20 18:21:44 +0800

    test

--- Library-Management/data.h
+++ Library-Management/data.h
@@ -10,7 +10,60 @@
 
 using namespace std;
 
-// Define a Book class
+class User {
+protected:
+    struct Users {
+        //string firstName, middleName, lastName;
+        string username;
+        string password;
+        //string address;
+        //int contactNo;
+        bool isAdmin;
+    };
+    vector<Users> usersList;
+
+    struct BorrowedBooks {
+        string username;
+        vector<int> ISBNs;
+    };
+    vector<BorrowedBooks> borrowedBooksList;
+
+public:
+    void serializeUsers();
+    void deserializeUsers();
+    void login(string username, string password);
+    bool userExists(string username);
+    void addUser(string username, string password, bool isAdmin);
+    void displayAnalytics();
+    void displayAllUsers();
+    void searchUser(string username);
+    void modifyUser(string username, string password);
+};
+
+class Book: public User {
+private:
+    struct Books {
+        string title;
+        string author;
+        long int ISBN;
+        int copies;
+    };
+    vector<Books> booksList;
+
+public:
+    void serializeBooks();
+    void deserializeBooks();
+    void addBook(string title, string author, long int ISBN, int copies);
+    void deleteBook(long int ISBN);
+    void modifyBook(long int ISBN);
+    void searchBook();
+    bool bookIsAvailable(long int ISBN);
+    void displayAllBooks();
+    void borrowBook(string username, long int ISBN);
+    void returnBook(string username, long int ISBN);
+};
+
+/*// Define a Book class
 class Book {
 public:
     string title;
@@ -28,6 +81,7 @@ public:
 void displayMainMenu();
 void searchBook(const vector<Book>& library, const string& title);
 void checkOutBook(vector<Book>& library, const string& title);
-void checkInBook(vector<Book>& library, const string& title);
+void checkInBook(vector<Book>& library, const string& title);*/
+
 
 #endif // DATA_H
--- Library-Management/functions.cpp
+++ Library-Management/functions.cpp
@@ -1,72 +1,67 @@
-//
-// Created by Admin on 9/19/2023.
-//
-
-
-
-
-
 #include "data.h"
 #include <iostream>
-
+#include <vector>
+#include <fstream>
+#include <iomanip>
 using namespace std; // Only added here, not in the header file
 
-// Function to display the main menu
-void displayMainMenu() {
-    cout << "===================================" << endl;
-    cout << "    Library Management System" << endl;
-    cout << "===================================" << endl;
-    cout << "[1] Search For a book" << endl;
-    cout << "[2] Check-in a book" << endl;
-    cout << "[3] Check-out a book" << endl;
-    cout << "[4] Inventory" << endl;
-    cout << "[5] Exit" << endl;
-}
+void User::serializeUsers() { //Write to users text file
+    ofstream usersFile("users.txt");
 
-// Function to search for a book by title
-void searchBook(const vector<Book>& library, const string& title) {
-    for (const Book& book : library) {
-        if (book.title == title) {
-            cout << "Book found:" << endl;
-            cout << "Title: " << book.title << endl;
-            cout << "Author: " << book.author << endl;
-            cout << (book.isAvailable ? "Status: Checked Out" : "Status: Available") << endl;
-            return;
+    if(usersFile.is_open()) {
+        for (auto & i : usersList) {
+            usersFile << i.username << "|" << i.password << "|" << i.isAdmin << endl;
         }
     }
-    cout << "Book not found." << endl;
+
+    usersFile.close();
 }
 
-// Function to check out a book by title
-void checkOutBook(vector<Book>& library, const string& title) {
-    for (Book& book : library) {
-        if (book.title == title) {
-            if (!book.isAvailable) {
-                book.isAvailable = true;
-                cout << "Book checked out successfully." << endl;
-                return;
-            } else {
-                cout << "This book is already checked out." << endl;
-                return;
+void User::deserializeUsers() { // Read from users text file
+    ifstream usersFile("users.txt");
+    string line, username, password;
+    bool isAdmin;
+
+    while (getline(usersFile, line)) {
+        size_t pos = line.find('|');
+
+        if (pos != string::npos) {
+            username = line.substr(0, pos);
+            line.erase(0, pos + 1); // nireremove neto yung |
+
+            pos = line.find('|');
+
+            if (pos != string::npos) {
+                password = line.substr(0, pos);
+                line.erase(0, pos + 1); // nireremove neto yung |
+
+                isAdmin = stoi(line);
+
+                addUser(username, password, isAdmin);
             }
         }
     }
-    cout << "Book not found." << endl;
+
+    usersFile.close();
 }
 
-// Function to check in a book by title
-void checkInBook(vector<Book>& library, const string& title) {
-    for (Book& book : library) {
-        if (book.title == title) {
-            if (book.isAvailable) {
-                book.isAvailable = false;
-                cout << "Book checked in successfully." << endl;
-                return;
-            } else {
-                cout << "This book is not checked out." << endl;
-                return;
-            }
+void User::addUser(string username, string password, bool isAdmin) {
+    Users users;
+
+    users.username = username;
+    users.password = password;
+    users.isAdmin = isAdmin;
+
+    usersList.push_back(users);
+}
+
+void User::displayAllUsers() {
+    if (usersList.empty())
+        exit(1);
+
+    else {
+        for (auto & i : usersList) {
+            cout << i.username << " | " << i.password << " | " << i.isAdmin << endl;
         }
     }
-    cout << "Book not found." << endl;
 }
